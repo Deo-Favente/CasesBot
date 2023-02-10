e34a7425f686be51bfb66ed62197e9b4078f85ac
Deo_Favente <56390262+Deo-Favente@users.noreply.github.com>
2023-02-11 00:25:54 +0100

    Update name

--- index.js
+++ index.js
@@ -2,9 +2,43 @@ const { Client, GatewayIntentBits } = require('discord.js')
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 const config = require('./config.json')
 
+const TOKEN = config.TOKEN
+const CLIENT_ID = config.CLIENT_ID
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+  
+    if (interaction.commandName === 'ping') {
+      await interaction.reply('Pong!');
+    }
+  });
+
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(config.TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
 client.login(config.TOKEN)
 
